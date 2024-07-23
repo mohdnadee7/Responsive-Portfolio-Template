46961efa5d07771a08bf2a5de77e007db63cf48e
Sarb <web2it@hotmail.co.uk>
2024-07-23 15:08:02 +0530

    css-changes-and-responsive

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,7 @@ function Contact() {
     return (
         <div className='contact-section pt-5'>
             <div classname="row">
-                <div className='resume-heading'>Contact Me</div>
+                <h1 className='heading'>Contact Me</h1>
                 <p className='text-center' style={{ color: "silver", padding: "0px 20px" }}>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             </div>
         </div>
--- src/components/skills.js
+++ src/components/skills.js
@@ -2,7 +2,7 @@ function Skills() {
     return (
         <div className='skill-section py-5'>
             <div classname="row">
-                <div className='resume-heading'>My Skills</div>
+                <h1 className='heading'>My Skills</h1>
                 <p className='text-center' style={{ color: "silver", padding: "0px 20px" }}>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
             </div>
             <div className="row mb-3">
