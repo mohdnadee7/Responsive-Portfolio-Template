2815b75f333ea5d88d9451fb4d2073da17ecbfef
Sarb <web2it@hotmail.co.uk>
2024-07-25 16:43:03 +0530

    header fixed

--- src/components/header.js
+++ src/components/header.js
@@ -4,22 +4,30 @@ import { faBars} from '@fortawesome/free-solid-svg-icons';
 function Header() {
   const [state, setState] = useState({
     className: 'hide',
-    property: 'false'
+    areaExpand: 'false'
   });
   const navbarToggler=()=>{
+if(state.areaExpand=='true' && state.className =='show'){
+  setState({
+    className: 'hide',
+    areaExpand: 'false'
+  });
+}
+else{
     setState({
       className: 'show',
-      property: 'true'
+      areaExpand: 'true'
     });
+  }
   }
     return (
         <nav class="navbar navbar-expand-lg">
         <div class="container">
           <a class="navbar-brand text-white" href="#home-section">Nadeem</a>
-          <button class="navbar-toggler" onClick={navbarToggler} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded='false' aria-label="Toggle navigation">
+          <button class="navbar-toggler" onClick={navbarToggler} type="button" data-bs-toggle="collapse" aria-controls="navbarNavAltMarkup" aria-expanded={state.areaExpand} aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} className="navbar-icon" />
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class={`collapse navbar-collapse ${state.className}`}>
             <div class="navbar-nav">
               <a class="nav-link custom-nav-link text-white" href="#">Home</a>
               <a class="nav-link custom-nav-link text-white" href="#about-section">About</a>
