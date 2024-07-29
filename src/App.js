5f8eb653732462788b847ab77615cd0f0667f53c
Sarb <web2it@hotmail.co.uk>
2024-07-29 12:37:54 +0530

    update font family

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Index from './components/index'
 import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
